33fe37bbce315262c5eb068d2d34192e084b6db5
simple.is.best0706@gmail.com <simple.is.best0706@gmail.com>
2026-05-20 01:00:29 +0900

    メッセージ送信機能追加

--- chatDisplay.js
+++ chatDisplay.js
@@ -14,14 +14,51 @@ lists.forEach(lists => {
             <div class="main-view">
                 <h2>${name}</h2>
                 <div class="message-area">
-                    <p>${name} の画面</p>
                 </div>
 
                 <div class="message-box">
                     <textarea class="message-input" placeholder="メッセージ"></textarea>
-                    <button class="send-btn">送信</button>
+                    <button class="send-btn" name="send-msg" >送信</button>
                 </div>
             </div>
         `;
+
+        //メッセージ送信機能部分の実装
+        //メイン画面のメッセージエリア取得
+        const messageArea = contentMain.querySelector(".message-area");
+
+        //メッセージの中身を取得
+        const messageInput = contentMain.querySelector(".message-input");
+
+        //送信ボタンの情報を取得
+        const sendBtn = contentMain.querySelector(".send-btn");
+
+        //メッセージ送信関数
+        function sendMessage(){
+            const text = messageInput.value.trim();
+            if(!text) return;
+
+            //divタグを作成し入社メッセージをぶち込む処理
+            const div = document.createElement("div");
+            div.textContent = text;
+            //divタグの下にテキストを追加
+            messageArea.appendChild(div);
+
+            //メッセージ送信後にテキストエリアを初期化
+            messageInput.value = "";
+
+        }
+
+        //メッセージ送信部分をクリックしたらsendMessage発動
+        sendBtn.addEventListener("click", sendMessage);
+        //Enterキーにも対応していくよーんっ
+        messageInput.addEventListener("keydown", (e) => {
+            //本家はEnterキーだけで送信されるので対抗でEnter＋Shiftで送信に^-^
+            if (e.key === "Enter" && e.shiftKey) {
+                e.preventDefault();
+                sendMessage();
+            }
+        });
+
     });
 });
